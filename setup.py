280c5894a61e1dfaf76d8b9d6ca3fb2972ca278d
Chris Mattmann <mattmann@apache.org>
2014-06-26 22:06:19 -0400

    - ignority - clean up and work with Translator API - fix JCC setup

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import sys
 from jcc import cpp
 
 options = {
-    'include': ('lib/tika-app-1.6-SNAPSHOT.jar','lib/org.eclipse.osgi.jar','lib/log4j.properties.jar'),
+    'include': ('lib/tika-app-1.6-SNAPSHOT.jar','lib/org.eclipse.osgi.jar','lib/log4j.properties.jar', 'lib/translator.microsoft.properties.jar', 'lib/microsoft-translator-java-api-0.6.2.jar', 'lib/json-simple-1.1.jar'),
     'jar': ('lib/tika-parsers-1.6-SNAPSHOT.jar', 'lib/tika-core-1.6-SNAPSHOT.jar', 'lib/tika-translate-1.6-SNAPSHOT.jar'),
-    'package': ('org.xml.sax',),
+    'package': ('org.xml.sax', 'java.lang', 'java.util', 'java.text', 'java.io'),
     'python': 'tika',
     'version': '1.6-SNAPSHOT',
-    #'module': 'translate',
+    'module': 'translate',
     'reserved': ('asm',),
-    'classes': ('java.io.File', 'java.io.FileInputStream', 'java.io.ByteArrayInputStream'),
+    'classes': ('java.io.File', 'java.io.FileInputStream', 'java.io.ByteArrayInputStream', 'java.lang.System','java.lang.Runtime','java.util.Arrays','java.util.Collections','java.util.HashMap','java.util.HashSet','java.util.TreeSet','java.lang.IllegalStateException','java.lang.IndexOutOfBoundsException','java.util.NoSuchElementException','java.text.SimpleDateFormat','java.text.DecimalFormat','java.text.Collator','java.io.StringReader','java.io.DataInputStream'),
 }
 
 import sys
