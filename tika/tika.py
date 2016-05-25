7cc2515f93b7a723127f57fa08565a26c31ef229
Chris Mattmann <mattmann@apache.org>
2016-05-25 11:04:10 -0400

    pin to 1.13, and prep for 1.13 release.

--- tika/tika.py
+++ tika/tika.py
@@ -90,7 +90,7 @@ from subprocess import STDOUT
 from os import walk
 
 Windows = True if platform.system() == "Windows" else False
-TikaVersion = os.getenv('TIKA_VERSION', '1.12')
+TikaVersion = os.getenv('TIKA_VERSION', '1.13')
 TikaJarPath = tempfile.gettempdir()
 TikaFilesPath = tempfile.gettempdir()
 TikaServerLogFilePath = tempfile.gettempdir()
