41da31cc9a5fb723b3139d2c11cdd226fa2bcfb9
Harsha Manjunatha <harsham05@gmail.com>
2016-06-22 11:45:31 -0700

    add versioning for tika-python library

--- tika/__init__.py
+++ tika/__init__.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__version__ = "1.13.1"
+
 try:
     __import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
