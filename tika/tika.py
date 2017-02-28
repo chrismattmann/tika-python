e67ecabefb2c08cd0ce0415f815551c802af95fd
Chris Mattmann <mattmann@apache.org>
2017-02-28 10:38:46 -0500

    Upgrade to 1.14 in prep for Tika Python 1.14 release. this closes #123

--- tika/tika.py
+++ tika/tika.py
@@ -118,7 +118,7 @@ log.addHandler(consoleHandler)
 log.setLevel(logging.INFO)
 
 Windows = True if platform.system() == "Windows" else False
-TikaVersion = os.getenv('TIKA_VERSION', '1.13')
+TikaVersion = os.getenv('TIKA_VERSION', '1.14')
 TikaJarPath = tempfile.gettempdir()
 TikaFilesPath = tempfile.gettempdir()
 TikaServerLogFilePath = tempfile.gettempdir()
