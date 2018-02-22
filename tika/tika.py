cb8cd4712bf2b74f90ee8b10683433ce0cfc132f
Chris Mattmann <chris.a.mattmann@jpl.nasa.gov>
2018-02-21 16:29:54 -0800

    This resolves #161: Latest version on PyPi.

--- tika/tika.py
+++ tika/tika.py
@@ -152,7 +152,7 @@ log.addHandler(consoleHandler)
 log.setLevel(logging.INFO)
 
 Windows = True if platform.system() == "Windows" else False
-TikaVersion = os.getenv('TIKA_VERSION', '1.15')
+TikaVersion = os.getenv('TIKA_VERSION', '1.16')
 TikaJarPath = os.getenv('TIKA_PATH', tempfile.gettempdir())
 TikaFilesPath = tempfile.gettempdir()
 TikaServerLogFilePath = log_path
