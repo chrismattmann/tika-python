7f342fcb41e29e5404ce6f3347db936d3229879e
Chris Mattmann <chris.mattmann@gmail.com>
2022-12-31 17:20:51 -0800

    Bump to version 2.6.0 for current development.

--- tika/__init__.py
+++ tika/__init__.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.25"
+__version__ = "2.6.0"
 
 try:
     __import__('pkg_resources').declare_namespace(__name__)
