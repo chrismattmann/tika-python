eb95428ded3a7a4edeb2db6ac7c82a00fec0786f
frankyang0635 <frankcisyang@FrankdeMacBook-Pro.local>
2025-03-24 17:48:39 -0700

    issue#413 solution

--- setup.py
+++ setup.py
@@ -88,6 +88,7 @@ setup(
     download_url='http://github.com/chrismattmann/tika-python',
     license='Apache License version 2 ("ALv2")',
     packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['tika'],
     include_package_data=True,
     zip_safe=True,
     test_suite='tika.tests',
