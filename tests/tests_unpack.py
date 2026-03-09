3fe18abada596fa9899d2f93298009be6985bf2c
Heinz-Alexander Fütterer <35225576+afuetterer@users.noreply.github.com>
2026-03-09 18:35:03 +0100

    test: convert unittest style tests to pytest test functions

--- tests/tests_unpack.py
+++ tests/tests_unpack.py
@@ -1,43 +1,42 @@
-# coding=utf8
-
-import unittest
 from tempfile import NamedTemporaryFile
+
 from tika import unpack
 
 
-class CreateTest(unittest.TestCase):
-    "Test different encodings"
-    text_utf8 = u"Hello, world!! 😎 👽"
-    text_ascii = u"Hello, world!!"
-
-    def test_utf8(self):
-        with NamedTemporaryFile("w+b", prefix='tika-python', suffix='.txt', dir='/tmp') as f:
-            f.write(self.text_utf8.encode("utf8"))
-            f.flush()
-            f.seek(0)
-            parsed = unpack.from_file(f.name)
-            self.assertEqual(parsed["content"].strip(), self.text_utf8)
-
-    def test_ascii(self):
-        with NamedTemporaryFile("w+t", prefix='tika-python', suffix='.txt', dir='/tmp') as f:
-            f.write(self.text_ascii)
-            f.flush()
-            f.seek(0)
-            parsed = unpack.from_file(f.name)
-            self.assertEqual(parsed["content"].strip(), self.text_ascii)
-
-    def test_from_buffer(self):
-        parsed = unpack.from_buffer('what?')
-        self.assertIsNotNone(parsed)
-        self.assertIsNotNone(parsed["metadata"])
-        self.assertEqual(parsed["metadata"]["Content-Length"], "5")
-
-    def test_from_buffer_with_headers(self):
-        parsed = unpack.from_buffer('what?', headers={'Param': 'whatever'})
-        self.assertIsNotNone(parsed)
-        self.assertIsNotNone(parsed["metadata"])
-        self.assertEqual(parsed["metadata"]["Content-Length"], "5")
+# Test data
+TEXT_UTF8 = "Hello, world!! 😎 👽"
+TEXT_ASCII = "Hello, world!!"
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_utf8():
+    """Test UTF-8 encoding"""
+    with NamedTemporaryFile("w+b", prefix="tika-python", suffix=".txt", dir="/tmp") as f:
+        f.write(TEXT_UTF8.encode("utf8"))
+        f.flush()
+        f.seek(0)
+        parsed = unpack.from_file(f.name)
+        assert parsed["content"].strip() == TEXT_UTF8
+
+
+def test_ascii():
+    """Test ASCII encoding"""
+    with NamedTemporaryFile("w+t", prefix="tika-python", suffix=".txt", dir="/tmp") as f:
+        f.write(TEXT_ASCII)
+        f.flush()
+        f.seek(0)
+        parsed = unpack.from_file(f.name)
+        assert parsed["content"].strip() == TEXT_ASCII
+
+
+def test_from_buffer():
+    parsed = unpack.from_buffer("what?")
+    assert parsed is not None
+    assert parsed["metadata"] is not None
+    assert parsed["metadata"]["Content-Length"] == "5"
+
+
+def test_from_buffer_with_headers():
+    parsed = unpack.from_buffer("what?", headers={"Param": "whatever"})
+    assert parsed is not None
+    assert parsed["metadata"] is not None
+    assert parsed["metadata"]["Content-Length"] == "5"
