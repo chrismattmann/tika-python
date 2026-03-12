11c2e6aea68a49afd0025666cb66ac43c5565174
Heinz-Alexander Fütterer <35225576+afuetterer@users.noreply.github.com>
2026-03-12 13:41:20 +0100

    test: add tests for tika.pdf

---
+++ tests/test_pdf.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from tika import pdf
+
+
+TEST_FILE_PATH = Path(__file__).parent / "files" / "rwservlet.pdf"
+
+
+def test_local_path():
+    text_pages = pdf.text_from_pdf_pages(str(TEST_FILE_PATH))
+    assert isinstance(text_pages, list)
