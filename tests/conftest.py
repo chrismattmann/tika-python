c9d66be62ee58718e772ee7c1f8f97051a904d32
Heinz-Alexander Fütterer <35225576+afuetterer@users.noreply.github.com>
2026-05-06 10:37:54 +0200

    style: apply pre-commit hooks

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,4 +8,3 @@ import pytest
 @pytest.fixture
 def test_file_path():
     return Path(__file__).parent / "files" / "rwservlet.pdf"
-
--- tika/tika.py
+++ tika/tika.py
@@ -903,4 +903,3 @@ if __name__ == '__main__':
     else:
         out.write(resp)
     out.write('\n')
-
