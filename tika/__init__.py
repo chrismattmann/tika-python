8ee4680a8fdf3fa294b26b07f847af316ffa4534
Chris Mattmann <chris.mattmann@gmail.com>
2026-08-01 12:12:45 -0700

    Start 3.3.3 development

--- tika/__init__.py
+++ tika/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "3.3.2"
+__version__ = "3.3.3.dev0"
 
 from pkgutil import extend_path
 
